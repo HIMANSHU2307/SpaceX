6cd5826bfdc0feb540cd70142b3129748126007a
HIMANSHU2307 <hvs2394@gmail.com>
2020-11-10 17:38:08 +0530

    Updated Interceptor

--- src/app/interceptor/service.interceptor.ts
+++ src/app/interceptor/service.interceptor.ts
@@ -1,4 +1,3 @@
-// import { ToastrService } from 'ngx-toastr';
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
@@ -7,22 +6,14 @@ import { Observable, throwError } from 'rxjs';
 @Injectable()
 export class Interceptor implements HttpInterceptor {
     constructor(
-      // private toastr: ToastrService
       ) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // const key = sessionStorage.getItem('userToken');
-        // const authKey = 'bearer ' + sessionStorage.getItem('userToken');
-        // if (authKey && key) {
-          // debugger;
             request = request.clone({
                 setHeaders: {
-                    // Authorization: authKey
                 }
             });
-        // }
-
-        return next.handle(request)
+            return next.handle(request)
             .pipe(
               catchError((error: HttpErrorResponse) => {
                 let errorMsg = '';
